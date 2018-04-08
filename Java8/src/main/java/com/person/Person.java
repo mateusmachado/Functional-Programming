abc3eac7dc10c6756492891337450a7d9432d39d
Mateus <mmachado.csp@gmail.com>
2018-04-08 18:47:10 -0300

    add  person class

---
+++ Java8/src/main/java/com/person/Person.java
@@ -0,0 +1,33 @@
+package com.person;
+
+public class Person {
+
+	String name;
+	int age;
+	String nationality;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+}
