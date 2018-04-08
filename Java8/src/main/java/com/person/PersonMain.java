95546b563abc115f7137faeeb923a533fb44c9e5
Mateus <mmachado.csp@gmail.com>
2018-04-08 19:30:31 -0300

    code restructured

--- Java8/src/main/java/com/person/PersonMain.java
+++ Java8/src/main/java/com/person/PersonMain.java
@@ -6,13 +6,20 @@ public class PersonMain {
 
 	public static void main(String[] args) {
 		Person person = new Person();
+		setPersonProperties(person);
+		Predicate<Person> adulthood = p -> p.getAge() > 18;
 
+		processAge(person, adulthood);
+	}
+
+	private static void setPersonProperties(Person person) {
 		person.setName("Matt");
 		person.setAge(24);
 		person.setNationality("Brazilian");
-			
-		Predicate<Person> compareAge = p -> p.getAge() > 18;
-		boolean b1 = compareAge.test(person);
-		System.out.println(b1 ? "Adult" : "Child");	
+	}
+
+	public static void processAge(Person person, Predicate<Person> predicate) {
+		if (predicate.test(person))
+			System.out.println(String.format("%s Adult.", person.getAge()));
 	}
 }
